f0804c4d0706f66f67507235908fe009df04cd41
swallow77 <57874611+swallow77@users.noreply.github.com>
2026-06-21 21:44:00 +0900

    Bump version to 0.2.5

--- custom_components/family_schedule_advisor/const.py
+++ custom_components/family_schedule_advisor/const.py
@@ -5,7 +5,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "family_schedule_advisor"
 NAME = "Family Schedule Advisor"
-VERSION = "0.2.4"
+VERSION = "0.2.5"
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.BUTTON]
 
